ccb314a9d2d394d801619012e7296c4477f249e8
Sojib Mondol <mdsojibmandol@gmail.com>
2024-08-05 21:59:14 +0600

    A. Legs codeforces round 962

---
+++ A_Legs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a;
+        cin>>a;
+        int result = (a-2)/4+1;
+        cout<<result<<endl;
+    }
+}
